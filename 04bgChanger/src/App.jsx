80ed6e7b0a1247cbc8a678cca94fa46daf9f92f2
Nikhil-bit21 <swaminikhil21@gmail.com>
2024-06-01 15:19:25 +0530

    optimised bgChanger

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -1,34 +1,22 @@
-import { useState } from "react"
-
+// App.js
+import { useState } from "react";
+import ColorButton from "./Components/ColorButton"
 
 function App() {
-  const [color, setColor] = useState("olive")
+  const [color, setColor] = useState("olive");
+  const colors = ["Red", "Green", "Black", "Blue", "Yellow", "Purple", "Orange", "Pink", "Brown", "Teal"];
 
   return (
-    <div className="w-full h-screen duration-200"
-    style={{backgroundColor: color}}
-    >
+    <div className="w-full h-screen duration-200" style={{ backgroundColor: color }}>
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
-        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
-          <button
-          onClick={() => setColor("red")}
-          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-          style={{backgroundColor: "red"}}
-          >Red</button>
-          <button
-          onClick={() => setColor("green")}
-          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-          style={{backgroundColor: "green"}}
-          >Green</button>
-          <button
-          onClick={() => setColor("blue")}
-          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-          style={{backgroundColor: "blue"}}
-          >Blue</button>
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-lg">
+          {colors.map((color) => (
+            <ColorButton key={color} color={color} onClick={() => setColor(color)} />
+          ))}
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
