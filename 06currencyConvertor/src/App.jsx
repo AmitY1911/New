09f23db6cd1ed06ecf1aeee52fdbfa4e07d1783d
Mukesh Utmani <109408900+mukeshutmani@users.noreply.github.com>
2024-07-30 17:50:18 +0500

    updated URL and Bug resolve

--- 06currencyConvertor/src/App.jsx
+++ 06currencyConvertor/src/App.jsx
@@ -46,7 +46,7 @@ function App() {
                             label="From"
                             amount={amount}
                             currencyOptions={options}
-                            onCurrencyChange={(currency) => setAmount(currency)}
+                            onCurrencyChange={(currency) => setAmount(amount)}
                             selectCurrency={from}
                             onAmountChange={(amount) => setAmount(amount)}
                         />
